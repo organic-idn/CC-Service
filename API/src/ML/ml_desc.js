418489f9da609f84aea74f82a9d0656ffdee0359
ahyarahmaguswir <79035093+ahyarahmaguswir@users.noreply.github.com>
2023-06-15 21:08:09 +0700

    Update ml_desc.js

--- API/src/ML/ml_desc.js
+++ API/src/ML/ml_desc.js
@@ -2,10 +2,20 @@
 mysql_db_ML = require("../database/db_sql.js")
 const pool = mysql_db.db_ML
 
+// Function to format text by removing newline characters
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.replace(/[\r\n]+/g, ' ');
+    }
+    return data;
+}
+
 module.exports = async (req, res) => {
     const scanningResult = req.query.result;
-
-    pool.query('SELECT info FROM organicdb WHERE nama = ?', [scanningResult], (error, results) => {
+    
+    const headData = ['ID', 'nama', 'namaLatin', 'imageURL', 'umur', 'deskripsi', 'nutrisi', 'penyimpanan'];
+    
+    pool.query('SELECT * FROM organicdb WHERE nama = ?', [scanningResult], (error, results) => {
         if (error) {
             console.error('Error retrieving organic info:', error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -16,9 +26,17 @@ module.exports = async (req, res) => {
         }
 
         else {
-            const organicInfo = results[0].info;
-            const formattedInfo = organicInfo.replace(/\\n/g, '\n');
-            res.send(formattedInfo);
+            const response = {
+                "ID": formatData(results[0][headData[0]]),
+                "nama":  formatData(results[0][headData[1]]),
+                "namaLatin":  formatData(results[0][headData[2]]),
+                "imageURL":  formatData(results[0][headData[3]]),
+                "umur":  formatData(results[0][headData[4]]),
+                "deskripsi":  formatData(results[0][headData[5]]),
+                "nutrisi":  formatData(results[0][headData[6]]),
+                "penyimpanan":  formatData(results[0][headData[7]]),
+            };
+            res.json(response);
         }
     });
 }
